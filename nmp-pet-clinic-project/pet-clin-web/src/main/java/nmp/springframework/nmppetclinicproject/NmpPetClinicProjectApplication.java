baebbce4df5ef3e43130efe812daee6f9c66eb48
Modith Prabath <mod9prabath@gmail.com>
2022-06-23 22:33:54 +0530

    added index pade and controller

--- nmp-pet-clinic-project/pet-clin-web/src/main/java/nmp/springframework/nmppetclinicproject/NmpPetClinicProjectApplication.java
+++ nmp-pet-clinic-project/pet-clin-web/src/main/java/nmp/springframework/nmppetclinicproject/NmpPetClinicProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NmpPetClinicProjectApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NmpPetClinicProjectApplication.class, args);
 	}
